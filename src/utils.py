af3fdf048d890a66e0b42fc1f90245d09e744f36
Dinislam36 <dingab36@gmail.com>
2024-05-09 21:58:42 +0300

    Minor fix

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ def fix_json(json_string):
                 del stack[-1]
             else:
                 if ch == '}':
-                    json_string = json_string[:idx - 1] + "]" + json_string[idx - 1:]
+                    json_string = json_string[:idx] + "]" + json_string[idx:]
                 else:
-                    json_string = json_string[:idx - 1] + "}" + json_string[idx - 1:]
+                    json_string = json_string[:idx] + "}" + json_string[idx:]
     return json_string
