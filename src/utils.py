b161e4436c08ea4ff17e4499759f8a3723bf8120
Dinislam36 <dingab36@gmail.com>
2024-05-10 00:18:33 +0300

    Implemented dict to markdown function

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,26 @@ def fix_json(json_string):
                 else:
                     json_string = json_string[:idx] + "}" + json_string[idx:]
     return json_string
+
+
+def dict_to_markdown(input_data, depth, prev_list=False):
+    if isinstance(input_data, str):
+        if prev_list:
+            return "* " + input_data + '\n'
+        return input_data + '\n'
+
+    if isinstance(input_data, list):
+        out_str = ''
+        for list_item in input_data:
+            out_str += dict_to_markdown(list_item, depth, prev_list=True)
+        return out_str
+
+    if isinstance(input_data, dict):
+        out_str = ''
+        for idx, key in enumerate(input_data.keys()):
+            if isinstance(input_data[key], dict) or isinstance(input_data[key], list):
+                out_str += "#" * depth + " " + str(key) + "\n"
+            if not isinstance(input_data[key], list) and idx == 0:
+                out_str += "#" * depth + " "
+            out_str += dict_to_markdown(input_data[key], depth + 1)
+        return out_str
