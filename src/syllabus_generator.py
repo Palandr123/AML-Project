af1f25e66bdfeb6b299dec235517f695c005c924
Dinislam36 <dingab36@gmail.com>
2024-05-10 13:03:40 +0300

    Bug fix

--- src/syllabus_generator.py
+++ src/syllabus_generator.py
@@ -136,10 +136,11 @@ def postprocess_json(string, field):
 
     string = str(string)
     string = fix_json(string)
-    string = string.split(f'"{field}": ')[-1]
+    # string = string.split(f'"{field}": ')[-1]
     start_index = min(string.find("["), string.find("{")) if string.find("[") != -1 else string.find("{")
     end_index = max(string.rfind("]"), string.rfind("}")) if string.rfind("]") != -1 else string.rfind("}")
     field_json = json.loads(string[start_index:end_index+1])
+    field_json = field_json[field]
     return field_json
 
 
